63c3f5d6203fa11f5008585be7f37428e9fa88fd
The-Graze <reece2004f@gmail.com>
2023-11-24 18:16:47 +0000

    Add project files.

---
+++ CustomRadarSounds/Patches/AudioPatch.cs
@@ -0,0 +1,24 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace CustomRadarSounds.Patches
+{
+    [HarmonyPatch(typeof(HUDManager))]
+    [HarmonyPatch("Start", MethodType.Normal)]
+    internal class AudioPatch
+    {
+        private static void Postfix(HUDManager __instance)
+        {
+            AudioThing.thing = __instance.gameObject.AddComponent<AudioThing>();
+            AudioThing.thing.Getaudio();
+        }
+    }
+}
---
+++ CustomRadarSounds/Plugin.cs
@@ -0,0 +1,73 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using GameNetcodeStuff;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace CustomRadarSounds
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<int> Audio;
+        Plugin() => Begin();
+
+        void Begin()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            Audio = Config.Bind("Settings", "AudioClip", 0, "Pick what number clip you would like to use");
+        }
+    }
+    public class AudioThing : MonoBehaviour
+    {
+        public static AudioThing thing;
+        string AudioPath = Path.Combine(Paths.PluginPath, PluginInfo.Name.ToString(), "Audio");
+        List<string> audioNames = new List<string>();
+        AudioClip audio;
+        public void Getaudio()
+        {
+            if (!Directory.Exists(AudioPath))
+            {
+                Directory.CreateDirectory(AudioPath);
+            }
+            string[] audio = Directory.GetFiles(AudioPath);
+            string[] audioname = new string[audio.Length];
+            for (int i = 0; i < audioname.Length; i++)
+            {
+                audioname[i] = Path.GetFileName(audio[i]);
+                audioNames.Add(audioname[i]);
+            }
+            StartCoroutine(LoadAudioCoroutine());
+        }
+        IEnumerator LoadAudioCoroutine()
+        {
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(Path.Combine(AudioPath , audioNames[Plugin.Audio.Value]), AudioType.UNKNOWN))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.Success)
+                {
+                    AudioClip audioClip = DownloadHandlerAudioClip.GetContent(www);
+                    audio = audioClip;
+                }
+                else
+                {
+                    Debug.LogError("Failed to load audio: " + www.error);
+                }
+            }
+        }
+        void Update()
+        {
+            if (audio != null)
+            {
+                HUDManager.Instance.scanSFX = audio;
+                Destroy(this);
+            }
+        }
+    }
+}
